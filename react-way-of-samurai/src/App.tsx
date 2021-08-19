1e186e32ec8279ed3f743dc2f726c23531c8f3bf
Ateo101 <nikmail242@gmail.com>
2021-08-19 20:36:05 +0900

    Initial commit

---
+++ react-way-of-samurai/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Technologies} from "./Technologies";
+import {Header} from "./Header";
+import {Footer} from "./Footer";
+
+function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
