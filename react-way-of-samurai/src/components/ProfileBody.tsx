408d99702556e294cf1ede737ed5f7fcb4a36010
Ateo101-Work <nikmail242@gmail.com>
2021-08-20 14:32:28 +0900

    Improved profile structure

--- react-way-of-samurai/src/components/ProfileBody.tsx
+++ react-way-of-samurai/src/components/ProfileBody.tsx
@@ -7,41 +7,65 @@ export function ProfileBody() {
     return (
         <div className={"ProfileBody"}>
             <ProfileAvatar/>
-            <ProfileAbout userName={"Alex"}/>
+            <ProfileAbout userName={"Alex"} aboutUser={"Some info about User"}/>
             <ProfileWritePost/>
             <ProfileMyPosts/>
         </div>
     )
 }
 
-function ProfileAvatar(){
+function ProfileAvatar() {
     return (
-        <div style={{ backgroundImage: `url(${banner})` }} className={"profile-banner-box"}>
+        <div style={{backgroundImage: `url(${banner})`}} className={"profile-banner-box"}>
             <img src={avatar} alt={"profile-avatar"} className={"profile-avatar"}/>
         </div>
     )
 }
-function ProfileAbout(props: ProfileAboutPropsType){
-    return (
-        <div>
-            <h2 className={"profile-name"}>I`m {props.userName}</h2>
-            <div>Обо мне</div>
-        </div>
-    )
-}
-function ProfileWritePost(){
-    return (
-        <div>
-            Написать пост
-        </div>
-    )
-}function ProfileMyPosts(){
-    return (
-        <div>
-            Мои посты
-        </div>
-    )
-}
+
 type ProfileAboutPropsType = {
     userName: string
+    aboutUser: string
+}
+
+function ProfileAbout(props: ProfileAboutPropsType) {
+    return (
+        <div className={"ProfileAbout"}>
+            <h2 className={"profile-name"}>I`m {props.userName}</h2>
+            <h3 className={"profile-about"}>Обо мне</h3>
+            <div>{props.aboutUser}</div>
+        </div>
+    )
+}
+
+function ProfileWritePost() {
+    return (
+        <div className={"ProfileWritePost"}>
+            <input/> <button>Написать пост</button>
+        </div>
+    )
+}
+
+type PostsListPropsType = {
+    userName: string
+    PostText: string
+}
+
+function ProfilePostsList(props: PostsListPropsType) {
+    return (
+        <div className={"ProfilePostsList"}>
+            <img src={avatar} alt={"profile-avatar"} className={"post-profile-avatar"}/>
+            <div className={"post-username"}>{props.userName}</div>
+            <div className={"post-text"}>{props.PostText}</div>
+        </div>
+    )
+}
+
+function ProfileMyPosts() {
+    return (
+        <div className={"ProfilePostsSection"}>
+            <div>Мои посты</div>
+            <ProfilePostsList userName={"Alex"} PostText={"My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post "}/>
+            <ProfilePostsList userName={"Alex"} PostText={"My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post My first post "}/>
+        </div>
+)
 }
