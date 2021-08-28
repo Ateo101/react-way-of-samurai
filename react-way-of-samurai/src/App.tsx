87fd220ba63ed020f9a6f685e2da8cfad51c6f06
Ateo101 <nikmail242@gmail.com>
2021-08-28 22:15:35 +0900

    Messages added

--- react-way-of-samurai/src/App.tsx
+++ react-way-of-samurai/src/App.tsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
-import {Sidebar} from "./components/Sidebar/Sidebar";
-import {ProfileBody} from "./components/ProfileBody/ProfileBody";
-import {Footer} from "./components/Footer/Footer";
+import {Navbar} from "./components/Navbar/Navbar";
+import {Profile} from "./components/Profile/Profile";
+import {BrowserRouter, Route} from 'react-router-dom';
+import Dialogs from './components/Dialogs/Dialogs';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+
 
 function App() {
     return (
-        <div className={"app-wrapper"}>
-            <Header/>
-            <Sidebar />
-            <ProfileBody/>
-            {/*<Footer/>*/}
-        </div>
+        <BrowserRouter>
+            <div className={"app-wrapper"}>
+                <Header/>
+                <Navbar/>
+                <div className={"app-wrapper-content"}>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/news" component={News}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
     );
 }
 
